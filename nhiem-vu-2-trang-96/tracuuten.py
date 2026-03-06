dd43cc563ba8c0647828c5e8d556789bb7e319c6
Van Tan Minh <vantanminh@privacy.miphira.dev>
2026-03-06 09:01:03 +0000

    Thêm dữ liệu điểm học sinh vào file diem.inp và cập nhật hàm tra cứu trong tracuuten.py

--- nhiem-vu-2-trang-96/tracuuten.py
+++ nhiem-vu-2-trang-96/tracuuten.py
@@ -1,5 +1,19 @@
 input_file = open("diem.inp", encoding="utf-8") #encoding để đọc được tiếng Việt có dấu
 ten_list = []
 diem_list = []
-for line in input_file:
-    
+for line in input_file.readlines():
+    ten,diem =line.split()
+    ten_list.append(ten)
+    diem_list.append(float(diem))
+ten=''
+while(ten!='end'):
+    ten = input('Nhập tên học sinh cần tra cứu, nhập từ khóa end để kết thúc:')
+    if (ten!='end'):
+        vitri=1
+        for i in range(0,len(ten_list)):
+            if ten==ten_list[i]:
+                vitri=i
+            if vitri==-1:
+                print('Khong tim thay hoc sinh trong danh sach')
+            else:
+                print('Diem so cua hoc sinh la:' ,diem_list[vitri])
